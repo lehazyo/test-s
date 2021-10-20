94273ef1d2f994c31135d5d47c22e66dbd4a4257
Алексей Сергиенко <lehazyo@gmail.com>
2021-10-20 20:06:14 +0300

    Стилевые правки по центральному блоку

--- src/components/app-upper/app-upper.jsx
+++ src/components/app-upper/app-upper.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React from "react";
+import styles from "./app-upper.module.css";
 import { AppCentral } from "../app-central/app-central";
 import { SideMenu } from "../side-menu/side-menu"
 
 export const AppUpper = () => {
   return (
-    <>
+    <section className={styles.wrapper}>
       <SideMenu />
       <AppCentral />
-    </>
+    </section>
   );
 };
