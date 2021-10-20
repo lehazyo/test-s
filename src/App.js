5a2e649ee04dc9e6a2aecd3f1a986222508a1131
Алексей Сергиенко <lehazyo@gmail.com>
2021-10-20 20:05:52 +0300

    Добавлен контекст

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import styles from './app.module.css';
 import './index.css';
 import './fonts.css';
@@ -12,5 +13,3 @@ export const App = () => {
     </div>
   );
 };
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
+import { App } from './app';
+
+const contextValue = {
+  currentUser: "vikavishny",
+  balance: "100.00",
+  notificationsCount: 1,
+};
+export const AppContext = createContext({});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppContext.Provider value={contextValue}>
+      <App />
+    </AppContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
